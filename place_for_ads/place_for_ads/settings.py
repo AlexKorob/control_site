e2ddd20eb9a543d67357f254036bc68d24718bfd
Alex <alexkorob96@gmail.com>
2019-03-24 12:38:48 +0200

    add pagination

--- place_for_ads/place_for_ads/settings.py
+++ place_for_ads/place_for_ads/settings.py
@@ -160,8 +160,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': (
     #     'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     # ),
-    # 'DEFAULT_PAGINATION_CLASS'​: 'rest_framework.pagination.LimitOffsetPagination'​,
-    # 'PAGINATE_BY': 10,
+    'DEFAULT_PAGINATION_CLASS': 'ads.pagination.Pagination',
 }
 
 SWAGGER_SETTINGS = {
