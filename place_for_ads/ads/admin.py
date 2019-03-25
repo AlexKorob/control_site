d64d119903a7a344e48f392bf1813359c299eddb
Alex <alexkorob96@gmail.com>
2019-03-25 17:03:00 +0200

    configure django_admin

--- place_for_ads/ads/admin.py
+++ place_for_ads/ads/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Ad, Category, Image
-from mptt.admin import DraggableMPTTAdmin
+from mptt.admin import DraggableMPTTAdmin, TreeRelatedFieldListFilter
 
 
 class AdAdmin(admin.ModelAdmin):
-    pass
+    list_display = [field.name for field in Ad._meta.fields]
+    list_filter = (("category", TreeRelatedFieldListFilter), )
+    search_fields = ("creator__username", "status", "title", "category__name")
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    pass
+class CategoryAdmin(DraggableMPTTAdmin):
+    list_filter=(("parent", TreeRelatedFieldListFilter), )
 
 
 class ImageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("ad", "image")
+    list_filter = (("ad__category", TreeRelatedFieldListFilter), )
+    search_fields = ("ad__creator__username", "ad__title")
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Ad, AdAdmin)
-admin.site.register(Category, DraggableMPTTAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Image, ImageAdmin)
